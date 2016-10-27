6cf8168679bd276998d85941cb0a306d34225e4f
TheElectronWill <theelectronwill@gmail.com>
2016-10-27 22:12:32 +0200

    Fix case statement

--- create-packages.sh
+++ create-packages.sh
@@ -13,7 +13,7 @@ if ! type 'rpmbuild' > /dev/null
 then
 	echo "You need the rpm development tools to create rpm packages"
 	read -p "Do you want to install rpmdevtools now? This will run sudo dnf install rpmdevtools. [y/N]" answer
-	case answer in
+	case $answer in
 		[Yy]* ) sudo dnf install rpmdevtools;;
 		* ) 
 			echo "Ok, I won't install rpmdevtools."
