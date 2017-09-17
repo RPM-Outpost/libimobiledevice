69d5827acb0e53554d53726002410b931c5ca149
TheElectronWill <theelectronwill@gmail.com>
2017-09-17 02:31:11 +0200

    Use the common utils

--- create-packages.sh
+++ create-packages.sh
@@ -1,34 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 # Author: TheElectronWill
 # This script downloads the latest source code of libimobiledevice, from github, and creates RPM packages.
 # The build architecture is chosen automatically.
+
+source terminal-colors.sh # Adds color variables
+source common-functions.sh # Adds utilities functions
+source basic-checks.sh # Checks that rpmbuild is available and that the script isn't started as root
 
 # Defines the needed paths
 rpm_dir=$PWD/RPMs
 work_dir=$PWD/work
 source_dir=$work_dir/SOURCES
 
-archive_file="$source_dir/libimobiledevice.tar.gz"
-
-# It's a bad idea to run rpmbuild as root!
-if [ "$(id -u)" = "0" ]; then
-	echo '----- WARNING -----'
-	echo 'This script should NOT be executed with root privileges!'
-	echo 'Building rpm packages as root is dangerous and may harm the system!'
-	echo 'Actually, badly written RPM spec files may execute dangerous command in the system directories.'
-	echo 'So it is REALLY safer not to run this script as root.'
-	echo 'If you still want to run this script as root, type "do it!" within 5 seconds (type anything else to exit):'
-	read -t 5 -n 6 -p 'Do you really want to do it (not recommended)? ' answer
-	if [ "$answer" != "do it!" ]; then
-		exit
-	fi
-fi
+archive_name='libimobiledevice.tar.gz'
+archive_file="$source_dir/$archive_name"
 
 # Installs the specified package with DNF
 install_package() {
 	echo "You need the $1 package."
-	read -n 1 -p "Do you want to install the $1 package now? [y/N]" answer
-	echo
+	ask_yesno "Install the $1 package now?"
 	case "$answer" in
 		y|Y)
 			sudo -p "Enter your password to install $1: " dnf install "$1"
@@ -38,13 +28,14 @@ install_package() {
 			fi
 			;;
 		*) 
-			echo "Ok, I won't install this package."
+			echo "The package won't be installed. Exiting now."
 			exit 1
 	esac
 }
 
 # Ensures that at least one of the specified packages is installed.
 ensure_available_or() {
+	set +e
 	for arg in "$@"; do
 		rpm -q "$arg" >/dev/null 2>&1
 		if [ $? -eq 0 ]; then
@@ -56,6 +47,7 @@ ensure_available_or() {
 
 # Ensures that all the specified packages are installed.
 ensure_available_and() {
+	set +e
 	for arg in "$@"; do
 		rpm -q "$arg" >/dev/null 2>&1
 		if [ $? -ne 0 ]; then
@@ -70,33 +62,9 @@ download_lib() {
 	wget -q --show-progress -O "$archive_file" 'https://github.com/libimobiledevice/libimobiledevice/archive/master.tar.gz'
 }
 
-# Asks the user if they want to remove the specified directory, and removes it if they want to.
-ask_remove_dir() {
-	read -n 1 -p "Do you want to remove the \"$1\" directory? [y/N]" answer
-	echo
-	case "$answer" in
-		y|Y)
-			rm -r "$1"
-			echo "\"$1\" directory removed."		
-			;;
-		*)
-			echo "Ok, I won't remove it."
-	esac
-}
-
-# If the specified directory exists, asks the user if they want to remove it.
-# If it doesn't exist, creates it.
-manage_dir() {
-	if [ -d "$1" ]; then
-		echo "The $2 directory already exist. It may contain outdated things."
-		ask_remove_dir "$1"
-	fi
-	mkdir -p "$1"
-}
-
 echo 'Checking requirements...'
 ensure_available_and 'rpmdevtools' 'usbmuxd' 'libtool' 'automake' 'autoconf' 'make'\
-	'gcc' 'pkgconfig' 'python-devel' 'libusbmuxd-devel' 'libplist-devel'\
+	'gcc' 'pkgconf-pkg-config' 'python2-devel' 'libusbmuxd-devel' 'libplist-devel'\
 	'python2-Cython' 'libplist-python'
 ensure_available_or 'openssl-devel' 'gnutls-devel'
 echo 'Requirements OK!'
@@ -107,12 +75,11 @@ manage_dir "$source_dir" 'sources'
 
 # Downloads the source in a tar.gz file
 if [ -f "$archive_file" ]; then
-	echo "Found $archive_file"
-	read -n 1 -p 'Do you want to use this archive instead of downloading a new one? [y/N]' answer
-	echo
+	echo "Found the archive \"$archive_name\"."
+	ask_yesno 'Do you want to use this archive instead of downloading a new one?' answer
 	case "$answer" in
 		y|Y)
-			echo 'Ok, I will use this archive.'
+			echo 'Existing archive selected.'
 			;;
 		*)
 			rm "$archive_file"
@@ -122,31 +89,26 @@ else
 	download_lib
 fi
 
+# Extracts the files:
+extract "$archive_file" "$work_dir"
+
 # Gets the last modification date of libimobiledevice to determine the package's version
 echo 'Analysing the files...'
-tar -xzf "$archive_file" -C "$work_dir"
 modif_date=$(date -r "$work_dir/libimobiledevice-master/README" +%Y.%m.%d)
-echo "The last modification of libimobiledevice was on $modif_date"
+disp "${green}The last modification of libimobiledevice was on $modif_date"
+style $reset
 
-# Chooses the spec file based on the system's architecture and build the packages
-echo 'Building RPM packages...'
+# Chooses the spec file based on the system's architecture and builds the packages
+set -e
+disp "${yellow}Creating the RPM packages (this may take a while)..."
 arch=$(uname -m)
 spec_file="libimobiledevice_$arch.spec"
 rpmbuild -bb --quiet "$spec_file" --define "_topdir $work_dir" --define "_rpmdir $rpm_dir"\
 	--define "release_number $modif_date"
 
-# Checks if there is an error
-if [ $? -ne 0 ]; then
-	echo '-----------'
-	echo 'Error!'
-	echo "Exit status: $?"
-	echo 'It seems like something went wrong :('
-else
-	echo '-----------'
-	echo 'Done!'
-	echo "The RPMs files are located in the \"RPMs/$arch\" folder."
-fi
+disp "${bgreen}Done!${reset_font}"
+disp "The RPM package is located in the \"RPMs/$arch\" folder."
+disp '----------------'
 
-# Removes directories if the user wants to
 ask_remove_dir "$work_dir"
-ask_remove_dir "$source_dir"
+ask_installpkg "all" "allowerasing"
